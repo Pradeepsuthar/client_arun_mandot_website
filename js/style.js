b952cecffd668aaaa34642928542530ab1aadbde
pradeepsuthar <sutharpradeep081@gmail.com>
2020-06-26 11:57:37 +0530

    added first commit

---
+++ js/style.js
@@ -0,0 +1,18 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    animateOut: 'fadeOut',
+    touchDrag  : false,
+    mouseDrag  : false
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
